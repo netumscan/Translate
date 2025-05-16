ce1816df8ae3563d32c3d8a7f1273326195a8250
Sean <soft@netumscan.com>
2025-05-16 15:01:44 +0800

    增加刷新翻译

--- locale.bat
+++ locale.bat
@@ -1,26 +1,26 @@
 cd ../Manual/Platform
 sphinx-build -b gettext source ../../Translate/Platform
 
-cd ../../Manual/Reader
+cd ../Reader
 sphinx-build -b gettext source ../../Translate/Reader
 
-cd ../../Manual/Wired-212
+cd ../Wired-212
 sphinx-build -b gettext source ../../Translate/Wired-212
 
-cd ../../Manual/Wired-280
+cd ../Wired-280
 sphinx-build -b gettext source ../../Translate/Wired-280
 
-cd ../../Manual/Wired-290
+cd ../Wired-290
 sphinx-build -b gettext source ../../Translate/Wired-290
 
-cd ../../Manual/Wired-CCD
+cd ../Wired-CCD
 sphinx-build -b gettext source ../../Translate/Wired-CCD
 
-cd ../../Manual/Wired-HS7
+cd ../Wired-HS7
 sphinx-build -b gettext source ../../Translate/Wired-HS7
 
-cd ../../Manual/Wireless
+cd ../Wireless
 sphinx-build -b gettext source ../../Translate/Wireless
 
-cd ../../Manual/Wireless-WiFi
+cd ../Wireless-WiFi
 sphinx-build -b gettext source ../../Translate/Wireless-WiFi
